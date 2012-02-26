c06d1adb61001ea24d0142b52006fa0a282c57a2
Bob Lail <bob.lailfamily@gmail.com>
2012-02-26 15:38:42 -0600

    added 'loginFields' for U.S. Bank

--- application/chrome/content/wesabe/fi-scripts/com/usbank.js
+++ application/chrome/content/wesabe/fi-scripts/com/usbank.js
@@ -1,6 +1,11 @@
 wesabe.download.Player.register({
   fid: 'com.usbank',
   org: 'US Bank',
+  
+  loginFields: [
+    {'label': 'User ID', 'type': 'text', 'key': 'username'},
+    {'label': 'Password', 'type': 'password', 'key': 'password'}
+  ],
 
   afterDownload: 'nextAccount',
 
